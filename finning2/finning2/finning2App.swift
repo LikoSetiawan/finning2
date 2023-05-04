d1d8e7d0ca6292b2cb9fbbb7fdabd24e48fe4c90
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2023-05-04 15:51:30 +0700

    Add files via upload

---
+++ finning2/finning2/finning2App.swift
@@ -0,0 +1,17 @@
+//
+//  finning2App.swift
+//  finning2
+//
+//  Created by Liko Setiawan on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct finning2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
